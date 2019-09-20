3bcf510eadbf0f4ed9248a8edefedcb800a1f13f
Nikki <nikki.satopdx@gmail.com>
2019-09-19 21:04:46 -0700

    updated

---
+++ app.js
@@ -0,0 +1,10 @@
+function testNumber () {
+    const numberInput = document.getElementById('number-input');
+    const resultParagraph = document.getElementById('result');
+
+    const number =  parseInt(numberInput.value)
+console.log(number)
+
+
+}
+window.testNumber = testNumber;
